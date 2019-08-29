d795d87fd0b2c903604fff9281a0660228c122fa
Atul Shakya <admin@Admins-MacBook-Pro-3.local>
2019-08-29 16:33:59 +0530

    creating new repo

---
+++ src/main/java/com/bharatpe/lending/Application.java
@@ -0,0 +1,15 @@
+package com.bharatpe.lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication
+//@PropertySources({ @PropertySource("file:/etc/bharatpe/key.properties"), })
+public class Application 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
