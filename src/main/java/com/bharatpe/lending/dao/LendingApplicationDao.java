045a8b850c92c051cd76aa7a0fa6cc124aae47ee
Atul Shakya <admin@Admins-MacBook-Pro-4.local>
2019-09-06 13:22:17 +0530

    update query added

--- src/main/java/com/bharatpe/lending/dao/LendingApplicationDao.java
+++ src/main/java/com/bharatpe/lending/dao/LendingApplicationDao.java
@@ -1,11 +1,18 @@
 package com.bharatpe.lending.dao;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bharatpe.common.entities.LendingApplication;
 
 @Repository
+@Transactional
 public interface LendingApplicationDao extends CrudRepository<LendingApplication , Long> {
+	@Modifying
+	@Query(value="update lending_application SET status = :newStatus WHERE id = :applicationId AND merchant_id = :merchantId AND status = :oldStatus",nativeQuery = true)
+	int updateApplicationStatus(String newStatus, Long applicationId, Long merchantId, String oldStatus);
 	LendingApplication fetchApplicationByIdAndStatus(Long applicationId, Long merchantId);
 }
