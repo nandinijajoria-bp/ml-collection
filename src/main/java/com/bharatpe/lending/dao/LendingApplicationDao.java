2183b688d14766c3120b3895147edf3a561e7b98
Atul Shakya <admin@Admins-MacBook-Pro-4.local>
2019-09-12 14:28:16 +0530

    dev properties and new function in lending application

--- src/main/java/com/bharatpe/lending/dao/LendingApplicationDao.java
+++ src/main/java/com/bharatpe/lending/dao/LendingApplicationDao.java
@@ -14,6 +14,10 @@ public interface LendingApplicationDao extends CrudRepository<LendingApplication
 	@Modifying
 	@Query(value="update lending_application SET status = :newStatus WHERE id = :applicationId AND merchant_id = :merchantId AND status = :oldStatus",nativeQuery = true)
 	int updateApplicationStatus(String newStatus, Long applicationId, Long merchantId, String oldStatus);
+	@Modifying
+	@Query(value="update lending_application SET status = :newStatus, agreement= 1, agreement_at = now() WHERE id = :applicationId AND merchant_id = :merchantId AND status in ( :oldStatus1, :oldStatus2) ",nativeQuery = true)
+	int updateApplicationStatusAndAgreement(String newStatus, Long applicationId, Long merchantId, String oldStatus1, String oldStatus2);
 	LendingApplication fetchApplicationByIdAndStatus(Long applicationId, Long merchantId);
 	LendingApplication findTop1ByMerchantIdOrderByApplicationIdDesc(Long merchantId);
+	LendingApplication findByApplicationId(Long applicationId);
 }
