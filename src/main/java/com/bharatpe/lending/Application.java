e2ace8ba06306e9e306d281c7b92c73803f15754
Atul Shakya <admin@Admins-MacBook-Pro-4.local>
2019-10-30 13:13:51 +0530

    property source path added

--- src/main/java/com/bharatpe/lending/Application.java
+++ src/main/java/com/bharatpe/lending/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages =  "com.bharatpe.*")
 @EntityScan(basePackages = "com.bharatpe.*")
 @EnableJpaRepositories(basePackages = "com.bharatpe.*")
+@PropertySource("file:/etc/bharatpe/production.properties")
 public class Application 
 {
 	public static void main(String[] args) {
