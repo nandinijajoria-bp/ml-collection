2a079ff6f3e1ac329ac28bf43dc8e9f3658ef561
sumit <sumit@192.168.0.104>
2020-01-03 15:59:53 +0530

    s3 bucked creds moved to properties

--- src/main/java/com/bharatpe/lending/constants/LendingConstants.java
+++ src/main/java/com/bharatpe/lending/constants/LendingConstants.java
@@ -9,11 +9,6 @@ public interface LendingConstants {
 	String GUPSHUP_VERIFY_OTP_METHOD= "TWO_FACTOR_AUTH";
 	String GUPSHUP_API_VERSION = "1.1";
 	
-	
-	String AWS_S3_ACCESS_KEY = "AKIA2INV2XVSLUEBQ4DH";
-	String AWS_S3_SECRET_KEY = "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";
-	String AWS_S3_BUCKET_NAME = "bharatpe-staging/lending-document";
-	
 	String X_KARZA_KEY = "IEPHvT1bUPTf4Ow";
 	
 	String KARZA_PAN_AUTHENTICATION_URL = "https://api.karza.in/v2/pan-authentication";
