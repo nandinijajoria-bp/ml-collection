1723f413f5591168557375e949ea83210dab2dda
raviranjan144 <56589689+raviranjan144@users.noreply.github.com>
2020-04-10 12:46:41 +0530

    cors

--- src/main/java/com/bharatpe/lending/interceptor/AccessControlInterceptor.java
+++ src/main/java/com/bharatpe/lending/interceptor/AccessControlInterceptor.java
@@ -9,7 +9,7 @@ public class AccessControlInterceptor extends HandlerInterceptorAdapter {
 	
 	@Override
 	 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		  response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//		  response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 		 response.setHeader("Access-Control-Allow-Origin", "*");
 	      response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, token, origin, authorization, accept, client-security-token");
 	      response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
