bb1332f38ab4eeda0fb2c8184551e56a41b0e858
Divya Prakash Singh <divya.prakash@bharatpe.com>
2020-07-27 16:14:34 +0530

    post test changes

--- src/main/java/com/bharatpe/lending/service/RedisNotificationService.java
+++ src/main/java/com/bharatpe/lending/service/RedisNotificationService.java
@@ -47,19 +47,19 @@ public class RedisNotificationService {
 			notificationDto.setMessage("Transfer Rs."+lendingApplication.getLoanAmount()+" to your "+bankName+" A/c Now!\n" + 
 					"Just 2 minutes to complete your loan application");
 			String messageString30min=objectMapper.writeValueAsString(notificationDto);
-			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString30min, "hashKey", 30*60);
+			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString30min, "applied_application_30min_"+lendingApplication.getId(), 30*60);
 			notificationDto.setMessage("Just 2 minutes away from Rs."+lendingApplication.getLoanAmount()+" Loan \n" + 
 					"Complete application now and get money in your "+bankName+" A/c");
 			String messageString1day=objectMapper.writeValueAsString(notificationDto);
-			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString1day, "hashKey", DateTimeUtil.getSecondsTillTime(13, 1));
+			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString1day, "applied_application_1day_"+lendingApplication.getId(), DateTimeUtil.getSecondsTillTime(13, 1));
 			notificationDto.setMessage("Complete your Loan Application in 2 Minutes! \n" + 
 					"Get Rs.<Loan Amount> in your <Bank Name> A/c  Now & Grow your Business.");
 			String messageString3day=objectMapper.writeValueAsString(notificationDto);
-			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString3day, "hashKey", DateTimeUtil.getSecondsTillTime(13, 3));
+			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString3day,"applied_application_3day_"+lendingApplication.getId(), DateTimeUtil.getSecondsTillTime(13, 3));
 			notificationDto.setMessage("Complete your Loan Application in 2 Minutes! \n" + 
 					"Get Rs.<Loan Amount> in your <Bank Name> A/c  Now & Grow your Business.");
 			String messageString5day=objectMapper.writeValueAsString(notificationDto);
-			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString5day, "hashKey", DateTimeUtil.getSecondsTillTime(13, 5));
+			delayedMessagePublisher.publish("application_applied", "partitionKey", messageString5day, "applied_application_5day_"+lendingApplication.getId(), DateTimeUtil.getSecondsTillTime(13, 5));
 		} catch (Exception e) {
 			logger.error("Error occured while sending notification {}",e);
 		}
