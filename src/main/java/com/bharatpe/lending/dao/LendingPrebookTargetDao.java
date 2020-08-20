4d0cc66bce5f0456b7b41cbc2c0bfd03c74fa9f5
khushal16 <khushal.virmani@bharatpe.com>
2020-08-20 18:00:23 +0530

    fix

--- src/main/java/com/bharatpe/lending/dao/LendingPrebookTargetDao.java
+++ src/main/java/com/bharatpe/lending/dao/LendingPrebookTargetDao.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -14,6 +15,7 @@ public interface LendingPrebookTargetDao extends JpaRepository<LendingPrebookTar
     LendingPrebookTarget findByMerchantIdAndApplicationId(Long merchantId, Long applicationId);
 
     @Modifying
+    @Transactional
     @Query(value = "UPDATE LendingPrebookTarget l set l.targetAchieveDate=:achievedDate,l.targetAchieved=true where l.merchantId=:merchantId")
     int updateTargetAchieved(Long merchantId, Date achievedDate);
 }
