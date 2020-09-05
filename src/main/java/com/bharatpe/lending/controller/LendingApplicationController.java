f449f9e850f644a0cf528245072175c3e11ea071
khushal16 <khushal.virmani@bharatpe.com>
2020-09-05 14:37:28 +0530

    fix

--- src/main/java/com/bharatpe/lending/controller/LendingApplicationController.java
+++ src/main/java/com/bharatpe/lending/controller/LendingApplicationController.java
@@ -140,7 +140,7 @@ public class LendingApplicationController {
 	
 	@RequestMapping(value = "/callLoanDetail", method = RequestMethod.GET)
 	public void callLoanDetails() {
-		callLoanDetailService.sendSMS();
+		callLoanDetailService.callLoanDetail();
 	}
 
 	@RequestMapping(value="/kafka/publish", method = RequestMethod.POST, consumes="application/json", produces="application/json")
