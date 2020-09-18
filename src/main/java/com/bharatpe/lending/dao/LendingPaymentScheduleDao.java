37685788d27223e527c5d65b3585fb2f5c1917bd
khushal16 <khushal.virmani@bharatpe.com>
2020-09-18 16:58:20 +0530

    fix

--- src/main/java/com/bharatpe/lending/dao/LendingPaymentScheduleDao.java
+++ src/main/java/com/bharatpe/lending/dao/LendingPaymentScheduleDao.java
@@ -18,7 +18,7 @@ public interface LendingPaymentScheduleDao extends CrudRepository<LendingPayment
 		
 	List<LendingPaymentSchedule> findByMerchantIdAndStatusAndCreditLoan(Long merchantId, String status, Boolean creditLoan);
 	
-	@Query(value="SELECT * FROM lending_payment_schedule WHERE merchant_id = :merchantId and status=:status and credit_loan=false",nativeQuery=true)
+	@Query(value="SELECT * FROM lending_payment_schedule WHERE merchant_id = :merchantId and status=:status and credit_loan=false order by id limit 1",nativeQuery=true)
 	LendingPaymentSchedule findByMerchantIdAndStatus(Long merchantId, String status);
 	
 	List<LendingPaymentSchedule> findByMerchantIdAndCreditLoanOrderByIdDesc(Long merchantId, Boolean creditLoan);
