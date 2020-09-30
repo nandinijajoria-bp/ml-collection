9fbef7fce67c3a91688db930adf92471727e3db3
khushal16 <khushal.virmani@bharatpe.com>
2020-09-30 11:04:22 +0530

    cors fix

--- src/main/java/com/bharatpe/lending/interceptor/AccessControlInterceptor.java
+++ src/main/java/com/bharatpe/lending/interceptor/AccessControlInterceptor.java
@@ -3,16 +3,18 @@ package com.bharatpe.lending.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+@Component
 public class AccessControlInterceptor extends HandlerInterceptorAdapter {
-	
+
 	@Override
-	 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 //		  response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-		 response.setHeader("Access-Control-Allow-Origin", "*");
-	      response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, token, origin, authorization, accept, client-security-token");
-	      response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-		  return true;
-	 }
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Headers", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+		return true;
+	}
 }
