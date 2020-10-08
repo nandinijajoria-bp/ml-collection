c8909146e4c43ce4827b4176545c4553b02677f1
harish <harish.meena@bharatpe.com>
2020-10-08 18:03:54 +0530

    minor update

--- src/main/java/com/bharatpe/lending/service/MerchantUpdateService.java
+++ src/main/java/com/bharatpe/lending/service/MerchantUpdateService.java
@@ -2,7 +2,6 @@ package com.bharatpe.lending.service;
 
 import com.bharatpe.common.dao.*;
 import com.bharatpe.common.entities.*;
-import com.bharatpe.common.objects.CommonAPIRequest;
 import com.bharatpe.common.utils.AesEncryption;
 import com.bharatpe.common.utils.HmacCalculator;
 import com.bharatpe.lending.constant.LendingConstants;
@@ -30,49 +29,38 @@ public class MerchantUpdateService {
 
 	@Autowired
 	private HmacCalculator hmacCalculator;
-	
-	@Autowired
-    AesEncryption aesEncryption;
 
-    @Autowired
+	@Autowired
+	AesEncryption aesEncryption;
+
+	@Autowired
 	InternalClientDao internalClientDao;
-	
+
 	@Autowired
-    RestTemplate restTemplate;
-
-	public Map<String, String> updateLoanInfoFromPanel(CommonAPIRequest commonAPIRequest) {
-		Map<String, String> finalResponse = new LinkedHashMap<>();
-		finalResponse.put("response", "success");
-		finalResponse.put("message", "loan Application updated");
-
-		// uploadDocToS3AndValidate(commonAPIRequest);
-
-		// updateLoanInfo(commonAPIRequest);
-
-		return finalResponse;
-	}
+	RestTemplate restTemplate;
 
 	public JsonNode curlMerchantPartialUpdateAPI(Long merchantId, List<PayloadDTO> payload) {
 		String response = null;
 		Map<String, String> paramMap = new LinkedHashMap<>();
 		paramMap.put("payload", reducePayloads(payload));
 		String hash = hmacCalculator.calculateHmac(hmacCalculator.getPayload(paramMap), getSecret("LENDING"));
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
 		headers.set("merchantId", merchantId.toString());
 		headers.set("hash", hash);
 		headers.set("Client-Name", "LENDING");
 
-        try {
-        	Instant start = Instant.now();
-        	HttpEntity<Map<String, String>> request = new HttpEntity<>(paramMap, headers);
-        	logger.info("Merchant Updated API request : {}", request);
-            response = restTemplate.postForObject(LendingConstants.MERCHANT_PARTIAL_UPDATE_URL, request, String.class);
-            logger.info("Merchant Updated API response : {}", response);
-            Instant end = Instant.now();
-    		logger.info("Time Taken by Merchant Updated API API : {} miliseconds", Duration.between(start, end).toMillis());
-        }catch(Exception e) {
-        	logger.info("Exception while Merchant Updated API API, Exception is ---", e);
+		try {
+			Instant start = Instant.now();
+			HttpEntity<Map<String, String>> request = new HttpEntity<>(paramMap, headers);
+			logger.info("Merchant Updated API request : {}", request);
+			response = restTemplate.postForObject(LendingConstants.MERCHANT_PARTIAL_UPDATE_URL, request, String.class);
+			logger.info("Merchant Updated API response : {}", response);
+			Instant end = Instant.now();
+			logger.info("Time Taken by Merchant Updated API API : {} miliseconds",
+					Duration.between(start, end).toMillis());
+		} catch (Exception e) {
+			logger.info("Exception while Merchant Updated API API, Exception is ---", e);
 		}
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode resp = null;
@@ -92,10 +80,10 @@ public class MerchantUpdateService {
 	}
 
 	private String getSecret(String clientName) {
-        InternalClient client = internalClientDao.findByClientName(clientName);
-        if (client != null) {
-            return aesEncryption.decrypt(client.getSecret());
-        }
-        return "";
-    }
+		InternalClient client = internalClientDao.findByClientName(clientName);
+		if (client != null) {
+			return aesEncryption.decrypt(client.getSecret());
+		}
+		return "";
+	}
 }
