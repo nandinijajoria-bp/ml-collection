a3d4f93eced5c5684e80f3e313435da016922516
Mihit Thakkar <56638093+mihit-thakkar@users.noreply.github.com>
2021-04-12 13:04:16 +0530

    Adding lender details in applicationStatus and merchantLoans (#371)
    added lender in status page/active loans

--- src/main/java/com/bharatpe/lending/service/LendingApplicationService.java
+++ src/main/java/com/bharatpe/lending/service/LendingApplicationService.java
@@ -2128,6 +2128,7 @@ public class LendingApplicationService {
 		applicationLoanDetailsDTO.setFailedMsg("");
 		applicationLoanDetailsDTO.setOrderID(lendingApplication.get().getExternalLoanId());
 		applicationLoanDetailsDTO.setTransferDays(tat < 1 ? "Soon" : tat + "-" + (tat+2) + " Days");
+		applicationLoanDetailsDTO.setLender(lendingApplication.get().getLender());
 		applicationLoanDetailsDTO.setStatus(lendingApplication.get().getStatus());
 		String modalType = null;
 		if (isLowPriority && showOrderQr && ("NTB".equals(lendingApplication.get().getLoanType()) || "NTB_SMS_1".equals(lendingApplication.get().getLoanType()))) {
