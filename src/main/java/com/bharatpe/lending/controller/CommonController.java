1f72cb10b8919cb311be6043c00ea2646215dee9
khushal16 <khushal.virmani@bharatpe.com>
2021-06-19 11:21:40 +0530

    removed hash api

--- src/main/java/com/bharatpe/lending/controller/CommonController.java
+++ src/main/java/com/bharatpe/lending/controller/CommonController.java
@@ -56,14 +56,14 @@ public class CommonController {
 		return pincodeVerify.checkPincodeValidity(pincode);
 	}
 
-	@RequestMapping(value="/hash", method=RequestMethod.POST)
-	public ResponseEntity<String> generateHash(@RequestBody Map<String, String> requestMap, @RequestHeader(name = "clientName") String clientName){
-		InternalClient internalClient = internalClientDao.findByClientName(clientName);
-		if (internalClient != null) {
-			logger.info("lending secret:{}", aesEncryption.decrypt(internalClient.getSecret()));
-			String hash = hmacCalculator.calculateHmac(hmacCalculator.getPayload(requestMap), aesEncryption.decrypt(internalClient.getSecret()));
-			return new ResponseEntity<>(hash, HttpStatus.OK);
-		}
-		return null;
-	}
+//	@RequestMapping(value="/hash", method=RequestMethod.POST)
+//	public ResponseEntity<String> generateHash(@RequestBody Map<String, String> requestMap, @RequestHeader(name = "clientName") String clientName){
+//		InternalClient internalClient = internalClientDao.findByClientName(clientName);
+//		if (internalClient != null) {
+//			logger.info("lending secret:{}", aesEncryption.decrypt(internalClient.getSecret()));
+//			String hash = hmacCalculator.calculateHmac(hmacCalculator.getPayload(requestMap), aesEncryption.decrypt(internalClient.getSecret()));
+//			return new ResponseEntity<>(hash, HttpStatus.OK);
+//		}
+//		return null;
+//	}
 }
