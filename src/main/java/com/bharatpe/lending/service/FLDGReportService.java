b3e155e7e881e1a045249af0bd3f311771f69fac
sachin-saraswat <80244693+sachin-saraswat@users.noreply.github.com>
2021-08-04 17:17:45 +0530

    fldg handling done (#453)
    * fldg handling done
    
    * fldg changes done
    
    Co-authored-by: saraswat000 <sachinsaraswat000@gmail.com>
    Co-authored-by: khushal16 <khushal.virmani@bharatpe.com>

---
+++ src/main/java/com/bharatpe/lending/service/FLDGReportService.java
@@ -0,0 +1,78 @@
+package com.bharatpe.lending.service;
+
+import com.bharatpe.common.entities.LendingApplication;
+import com.bharatpe.common.entities.LendingPaymentSchedule;
+import com.bharatpe.lending.common.dao.LendingFLDGDao;
+import com.bharatpe.lending.common.entity.LendingFLDG;
+import com.bharatpe.lending.common.util.DateTimeUtil;
+import com.bharatpe.lending.dao.LendingApplicationDao;
+import com.bharatpe.lending.dao.LendingPaymentScheduleDao;
+import com.bharatpe.lending.handlers.S3BucketHandler;
+import com.bharatpe.lending.util.LoanUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.Objects;
+
+@Service
+public class FLDGReportService {
+
+    Logger logger = LoggerFactory.getLogger(FLDGReportService.class);
+
+    @Autowired
+    S3BucketHandler s3BucketHandler;
+
+    @Autowired
+    LendingPaymentScheduleDao lendingPaymentScheduleDao;
+
+    @Autowired
+    LendingApplicationDao lendingApplicationDao;
+
+    @Autowired
+    LendingFLDGDao lendingFLDGDao;
+
+    public void uploadFLDGReportEntries(String fileName) {
+        try {
+            LendingFLDG lendingFLDG = new LendingFLDG();
+            logger.info("Getting file : {} from s3", fileName);
+            InputStream fldgFile = s3BucketHandler.getObject(fileName, "loan-document");
+            BufferedReader fldgFileReader = new BufferedReader(new InputStreamReader(fldgFile));
+            String readLine = fldgFileReader.readLine();
+            readLine = fldgFileReader.readLine();
+            Date startDate = DateTimeUtil.getCurrentDayStartTime();
+            while(Objects.nonNull(readLine)) {
+                logger.info("readline: {}", readLine);
+                String[] arr = readLine.split(",");
+                String externalLoanId = arr[1];
+                LendingApplication lendingApplication = lendingApplicationDao.findByExternalLoanId(externalLoanId);
+
+                Double fldgAmount = Double.valueOf(arr[2]);
+                Double fldgInterest = Double.valueOf(arr[2]);
+                Double fldgPrinciple = Double.valueOf(arr[3]);
+                if(Objects.nonNull(lendingApplication)) {
+                    LendingPaymentSchedule lendingPaymentSchedule = lendingPaymentScheduleDao.findByMerchantIdAndApplicationId(lendingApplication.getMerchant().getId(), lendingApplication.getId());
+                    if (Objects.nonNull(lendingPaymentSchedule)) {
+                        lendingFLDG.setLoanId(lendingPaymentSchedule.getId());
+                        lendingFLDG.setFldgAmount(fldgAmount);
+                        lendingFLDG.setFldgInterest(fldgInterest);
+                        lendingFLDG.setFldgPrinciple(fldgPrinciple);
+                        lendingFLDG.setPreFldgPaidAmount(lendingPaymentSchedule.getPaidAmount());
+                        lendingFLDG.setPreFldgPaidInterest(lendingPaymentSchedule.getPaidInterest());
+                        lendingFLDG.setPreFldgPaidPrinciple(lendingPaymentSchedule.getPaidPrinciple());
+                        lendingFLDG.setFldgDate(startDate);
+                        lendingFLDGDao.save(lendingFLDG);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while saving fldg report: {}",e);
+        }
+    }
+
+}
