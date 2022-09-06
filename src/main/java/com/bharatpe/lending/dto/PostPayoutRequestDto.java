c8d4984ec02b0c8a57f3ccd7904c58cbbe3a6eb3
happy-bharatpe <happy.nawani@bharatpe.com>
2022-09-06 11:06:51 +0530

    Update PostPayoutRequestDto.java

--- src/main/java/com/bharatpe/lending/dto/PostPayoutRequestDto.java
+++ src/main/java/com/bharatpe/lending/dto/PostPayoutRequestDto.java
@@ -1,5 +1,6 @@
 package com.bharatpe.lending.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -23,6 +24,7 @@ public class PostPayoutRequestDto {
     @JsonProperty("status")
     String loanDisbursalStatus;
     @JsonProperty("timestamp")
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Kolkata")
     Date disbursalDate;
     @JsonProperty("bp_loan_id")
     String applicationId;
