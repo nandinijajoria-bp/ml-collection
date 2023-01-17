cc8ecba2cb1d4e6fcd92da6b373e8c18e4a30ee2
code-rajat <rajat.singh@bharatpe.com>
2023-01-17 16:32:26 +0530

    lender pg mid changes

--- src/main/java/com/bharatpe/lending/service/APIGatewayService.java
+++ src/main/java/com/bharatpe/lending/service/APIGatewayService.java
@@ -282,6 +282,10 @@ public class APIGatewayService {
         logger.info("In Create pg transaction for merchnat id {}", merchantId);
         InternalClientSlave internalClient = internalClientDaoSlave.findByClientName(CLIENT);
         LendingPgMidConfigSlave pgMidConfig = lendingPgMidConfigSlaveDao.findByNameAndStatus(pgCreateTransactionRequestDTO.getLender().name(), "ACTIVE");
+        if (loanUtil.isInternalMerchant(merchantId) && (Lender.LIQUILOANS.name().equals(pgCreateTransactionRequestDTO.getLender().name())
+                || Lender.LIQUILOANS_P2P_OF.name().equals(pgCreateTransactionRequestDTO.getLender().name()) || Lender.ABFL.name().equals(pgCreateTransactionRequestDTO.getLender().name()))) {
+            pgMidConfig = lendingPgMidConfigSlaveDao.findByNameAndStatus(pgCreateTransactionRequestDTO.getLender().name(), "INACTIVE");
+        }
         logger.info("pg config related to mid: {}", pgMidConfig);
         try {
             Map requestParams = new HashMap<>();
