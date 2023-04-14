016216c422a1686bd1e1a3fbf511aa1b9c57ce0f
Divya Sharma <divya.sharma@bharatpe.com>
2023-04-14 14:53:27 +0530

    ediModel added logs

--- src/main/java/com/bharatpe/lending/interceptor/InterceptorAppConfig.java
+++ src/main/java/com/bharatpe/lending/interceptor/InterceptorAppConfig.java
@@ -48,7 +48,7 @@ public class InterceptorAppConfig implements WebMvcConfigurer {
 						"/lending/check_loan_status", "/support/cancelApplication",  "/lending/getLoanDashboardDetails", "/lending/nbfc/mamta/decision/callback",
 						"/assign/lender","/assign/rules", "/assign/limit","/assign/update-rule","/assign/update-limit","/support/computeEligibility", "/lending/payment/ledger_entry",
 						"/lending/v3/modify/application", "/lending/v3/callback/bre", "/lending/v3/callback/kyc", "/lending/v3/callback/drawdown", "/lending/v3/callback/sanction",
-						"/lending/v3/modifyLender", "/lending/v3/nextStage", "/lending/v3/callback/invoke/dataUpload", "/support/sendArcCommunication");
+						"/lending/v3/modifyLender", "/lending/v3/nextStage", "/lending/v3/callback/invoke/dataUpload", "/support/sendArcCommunication", "/lending/v3/callback/*", "/lending/v3/test/**");
 
 
         registry.addInterceptor(clientHmacInterceptor).addPathPatterns("/lending/internal/**","/lending/first_loan_status", "/lending/check_loan_status", "/lending/pullPayment",
