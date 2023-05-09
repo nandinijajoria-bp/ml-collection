822cf5ab611bc3d0e685ceb4574ead556eadf7d4
kirti2930 <kirti.gupta@bharatpe.com>
2023-05-09 18:00:19 +0530

    did optional dao changes

--- src/main/java/com/bharatpe/lending/dao/AutoPayUPIDao.java
+++ src/main/java/com/bharatpe/lending/dao/AutoPayUPIDao.java
@@ -3,9 +3,11 @@ package com.bharatpe.lending.dao;
 import com.bharatpe.lending.entity.AutoPayUPIEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AutoPayUPIDao extends JpaRepository<AutoPayUPIEntity, Long> {
 
-    AutoPayUPIEntity findByMerchantIdAndApplicationId(Long merchantId, Long applicationId);
+    Optional<AutoPayUPIEntity> findByMerchantIdAndApplicationId(Long merchantId, Long applicationId);
 
     AutoPayUPIEntity findByMerchantIdAndOrderId(Long merchantId, String orderId);
 
