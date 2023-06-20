41bb4fc3af6259de77d7d15d51e5cf5865ab7e3c
Sanskarrawat <105841194+Sanskarrawat@users.noreply.github.com>
2023-06-20 18:35:33 +0530

    fix: topup v3 fix

--- src/main/java/com/bharatpe/lending/service/MerchantLoansService.java
+++ src/main/java/com/bharatpe/lending/service/MerchantLoansService.java
@@ -722,10 +722,11 @@ public class MerchantLoansService {
                     logger.info("No topup eligibility found for merchant:{}", lendingPaymentSchedule.getMerchantId());
                     return eligiblity;
                 }
+                eligibleAmount = Math.min(eligibleAmount, lendingPaymentSchedule.getLoanAmount());
                 if (!excludeTopUpBaseChecks(lendingPaymentSchedule.getMerchantId())) {
                     if (ediPaidRatio < 50D) {
                         logger.info("EDI paid ratio:{} is less than 50% for merchant:{}", ediPaidRatio, lendingPaymentSchedule.getMerchantId());
-                        eligibleAmount = Math.min(eligibleAmount, lendingPaymentSchedule.getLoanAmount());
+                        return eligiblity;
                     }
                     int posAmount = loanUtil.getForeclosureAmount(lendingPaymentSchedule);
                     if (eligibleAmount - posAmount < 10000) {
