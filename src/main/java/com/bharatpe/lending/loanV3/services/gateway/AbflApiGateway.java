32735744db986b19bbcc532b9bbe684b21d14c92
Peeyush1729 <110889333+Peeyush1729@users.noreply.github.com>
2024-04-16 12:59:45 +0530

    variable name correction

--- src/main/java/com/bharatpe/lending/loanV3/services/gateway/AbflApiGateway.java
+++ src/main/java/com/bharatpe/lending/loanV3/services/gateway/AbflApiGateway.java
@@ -50,7 +50,7 @@ public class AbflApiGateway extends INbfcLenderGateway {
     @Value("${nbfc.foreclosureamt.api:api/v3/lender/foreclosure-details}")
     String nbfcForeClosureAmtUrl;
 
-    @Value("${nbfc.rps.api:api/v3/lender/digital-sign}")
+    @Value("${nbfc.digitalSign.api:api/v3/lender/digital-sign}")
     String nbfcDigitalSignUrl;
 
     @Value("${nbfc.rps.api:api/v3/lender/repayment-schedule}")
