f33c977e68a7b7267dde02a23e6a8396ac61f2b2
namanmittal <naman.mittal@bharatpe.com>
2024-05-10 15:49:52 +0530

    added : intial package addition

---
+++ src/main/java/com/bharatpe/lending/collection/LoanStatusService.java
@@ -0,0 +1,7 @@
+package com.bharatpe.lending.collection;
+
+public interface LoanStatusService {
+    public void processLoanClosure();
+    public boolean checkLoanClosure();
+
+}
---
+++ src/main/java/com/bharatpe/lending/collection/impl/LoanStatusServiceImpl.java
@@ -0,0 +1,16 @@
+package com.bharatpe.lending.collection.impl;
+
+import com.bharatpe.lending.collection.LoanStatusService;
+
+public class LoanStatusServiceImpl implements LoanStatusService {
+    @Override
+    public void processLoanClosure() {
+
+    }
+
+    @Override
+    public boolean checkLoanClosure() {
+        return false;
+    }
+
+}
