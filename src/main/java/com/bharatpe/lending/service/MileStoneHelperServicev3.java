c7ed7390359c9ac1783ae52a52df25df10929e6e
Muskan-Parveen <muskan.parveen@bharatpe.com>
2024-07-17 16:44:58 +0530

    closed session handling

--- src/main/java/com/bharatpe/lending/service/MileStoneHelperServicev3.java
+++ src/main/java/com/bharatpe/lending/service/MileStoneHelperServicev3.java
@@ -350,6 +350,10 @@ public class MileStoneHelperServicev3 {
             log.info("LRV not found for merchantId: {}", merchant.getId());
             return false;
         }
+        if(RTESessionStatus.CLOSED.name().equalsIgnoreCase(entity.getSessionStatus())) {
+            log.info("Session already closed for merchantId: {}", merchant.getId());
+            return false;
+        }
 
         List<String> inclusionReasonMilestoneList = getInclusionReasonMilestoneList();
 
