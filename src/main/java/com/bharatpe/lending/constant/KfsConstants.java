bffe1893452b124e892ef157460d78f0c28ababa
chinmay-bharatpe <chinmay.sahani.ext@bharatpe.com>
2024-11-20 17:52:22 +0530

    feature-MLI-757 (#2660)
    * Nodal Officer Changes
    
    * KFS Fix
    
    * Vernacular Fix

--- src/main/java/com/bharatpe/lending/constant/KfsConstants.java
+++ src/main/java/com/bharatpe/lending/constant/KfsConstants.java
@@ -52,9 +52,9 @@ public interface KfsConstants {
     String COLENDER_BUSINESS_ADDRESS_MAMTA2 = "B-17, 4th Floor, Art Guild House, Phoenix Market City, Kurla West, Mumbai, Mumbai City MH 400070, India";
 
 
-    String LSP_CONTACT_NAME = "Mr. Ankit Parnami";
+    String LSP_CONTACT_NAME = "Ms. Mallika Janardhan";
     String LSP_CONTACT_EMAIL = "nodalofficer@bharatpemoney.com";
-    String LSP_CONTACT_NUMBER = "+91-7289000552";
+    String LSP_CONTACT_NUMBER = "+91-9899355922";
 
     Double PROCESSING_FEE_PERCENTAGE = 2.0;
 
@@ -95,7 +95,7 @@ public interface KfsConstants {
     String LENDER_BUSINESS_ADDRESS_CREDITSAISON = "IndiQube Lexington Tower, First Floor, Tavarekere Main Rd, Tavarekere, S.G. Palya, Bengaluru, Karnataka 560029";
     String LENDER_CONTACT_NAME_CREDITSAISON = "Emaad Khan";
     String LENDER_CONTACT_EMAIL_CREDITSAISON = "emaad.khan@creditsaison-in.com";
-    String LENDER_CONTACT_NUMBER_CREDITSAISON = "+91-7289000552";
+    String LENDER_CONTACT_NUMBER_CREDITSAISON = "+91-9899355922";
     String LENDER_CORPORATE_NAME_SMFG = "SMFG India Credit Company Limited (formerly Fullerton India Credit Company Limited)";
     String LENDER_BUSINESS_ADDRESS_SMFG = "Commerzone IT Park, Tower B, 1st Floor No.111, Mount Poonamallee Road, Porur, Chennai, Tamil Nadu. Pincode - 600116";
     String LENDER_CONTACT_NAME_SMFG = "Grievance Redressal Officer";
