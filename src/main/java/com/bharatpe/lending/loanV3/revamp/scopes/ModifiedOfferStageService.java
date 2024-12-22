d8f1cc34434722fe2aa2540b31b0750947dbeef6
vineetkumarbharatpe <105650957+vineetkumarbharatpe@users.noreply.github.com>
2024-12-22 18:03:15 +0530

    Update ModifiedOfferStageService.java

--- src/main/java/com/bharatpe/lending/loanV3/revamp/scopes/ModifiedOfferStageService.java
+++ src/main/java/com/bharatpe/lending/loanV3/revamp/scopes/ModifiedOfferStageService.java
@@ -62,8 +62,7 @@ public class ModifiedOfferStageService implements IStageDataService<ModifiedOffe
             double initialDisbursalAmountWithoutProcessingFee = lendingApplication1.getDisbursalAmount() + lendingApplication1.getProcessingFee();
             double  processingFeeRate = lendingApplication1.getProcessingFee()/initialDisbursalAmountWithoutProcessingFee;
             double processingFee = Math.ceil(lendingApplicationLenderDetails.getNbfcApprovedLoanOfferAmt() * processingFeeRate);
-            Double apr = lendingApplicationServiceV2.getApr(lendingApplication1.getMerchantId(), lendingApplication1.getId(), lendingApplicationLenderDetails.getNbfcApprovedLoanOfferAmt() - processingFee,
-                           LenderOffDays.valueOf(lendingApplication1.getLender()).getEdiModel().getNoOfEdiDaysInAWeek(), lendingApplication1.getLender());
+            Double apr = lendingApplicationServiceV2.getApr(Math.toIntExact(lendingApplication1.getPayableDays()),lendingApplication1.getEdi(), lendingApplicationLenderDetails.getNbfcApprovedLoanOfferAmt() - processingFee, lendingApplication1.getMerchantId());
            ModifiedOfferStateDTO modifiedOfferStateDTO = ModifiedOfferStateDTO.builder().
                    loanOffer(lendingApplicationLenderDetails.getNbfcApprovedLoanOfferAmt())
                    .interestRate(lendingApplication1.getInterestRate())
