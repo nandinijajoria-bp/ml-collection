4ebe39ce9f83990794f0613dcd0feb0264625264
Vyom Singh <Vyom.Singh@VyomSingh.local>
2025-04-22 11:42:24 +0530

    feat: feature-ML-1575

--- src/main/java/com/bharatpe/lending/service/MileStoneProgramService.java
+++ src/main/java/com/bharatpe/lending/service/MileStoneProgramService.java
@@ -17,6 +17,7 @@ import com.bharatpe.lending.common.query.entity.MileStoneRewardSlave;
 import com.bharatpe.lending.common.query.entity.MileStoneSlave;
 import com.bharatpe.lending.common.service.FunnelService;
 import com.bharatpe.lending.common.service.merchant.dto.BasicDetailsDto;
+import com.bharatpe.lending.common.service.merchant.service.MerchantService;
 import com.bharatpe.lending.common.util.DateTimeUtil;
 import com.bharatpe.lending.common.util.EasyLoanUtil;
 import com.bharatpe.lending.common.util.MapperUtil;
@@ -112,6 +113,9 @@ public class MileStoneProgramService {
 
     @Autowired
     LoanDashboardService loanDashboardService;
+
+    @Autowired
+    MerchantService merchantService;
 
     @Autowired
     LendingEligibleLoanDao eligibleLoanDao;
@@ -228,6 +232,23 @@ public class MileStoneProgramService {
         if (ObjectUtils.isEmpty(kycPancard)) {
             return new ApiResponse<>(false, "400", "PANCARD_NOT_FOUND");
         }
+
+        if (merchant.getMobile() != null && !merchant.getMobile().isEmpty()) {
+            log.info("Mobile number already set for merchantId: {}", merchant.getId());
+        } else {
+            Optional<BasicDetailsDto> basicDetailsDto = merchantService.fetchMerchantBasicDetails(merchant.getId());
+            if (basicDetailsDto.isPresent()) {
+                BasicDetailsDto details = basicDetailsDto.get();
+                String mobile = details.getMobile();
+                if (mobile != null && !mobile.isEmpty()) {
+                    log.info("mobile_no {} for merchantId: {}", mobile, merchant.getId());
+                    merchant.setMobile(mobile);
+                } else {
+                    log.warn("Mobile number is null or empty for merchantId: {} and details: {}", merchant.getId(),details);
+                }
+            }
+        }
+
         BureauResponseDTO responseDTO = mileStoneHelperService.calculateBureauScore(kycPancard, merchant);
 
         log.info("bureau data {} for merchant id {} is :", responseDTO, merchant.getId());
