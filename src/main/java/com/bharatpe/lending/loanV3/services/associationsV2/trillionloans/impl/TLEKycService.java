f5f2f801558dee0fe6de83fbf7575d1bf0638a04
grace.garg <grace.garg@bharatpemoney.com>
2025-06-20 15:03:12 +0530

    MLI-1616: Add null check before processing NBFC response

--- src/main/java/com/bharatpe/lending/loanV3/services/associationsV2/trillionloans/impl/TLEKycService.java
+++ src/main/java/com/bharatpe/lending/loanV3/services/associationsV2/trillionloans/impl/TLEKycService.java
@@ -148,7 +148,7 @@ public class TLEKycService {
                 if (!ObjectUtils.isEmpty(eKycCallbackResponse) && Arrays.asList("approval_pending", "approved", "success").contains(eKycCallbackResponse.getPayload().getKycRequest().getStatus())) {
                     NBFCRequestDTO<?> eKycStatusRequestDto = getEKycStatusCheckPayload(lendingApplication, lendingApplicationLenderDetails, false);
                     NBFCResponseDTO<?> nbfcResponseDto = lenderAPIGateway.invokeStage(eKycStatusRequestDto, LenderAssociationStages.EKYC_STATUS);
-                    if (nbfcResponseDto.getSuccess() && !ObjectUtils.isEmpty(nbfcResponseDto.getData())) {
+                    if (nbfcResponseDto != null && nbfcResponseDto.getSuccess() && !ObjectUtils.isEmpty(nbfcResponseDto.getData())) {
                         TLEKycStatusCheckResponseDto ekycStatusCheckResponseDto = objectMapper.convertValue(nbfcResponseDto.getData(), TLEKycStatusCheckResponseDto.class);
                         if (processEKycStatusResponse(ekycStatusCheckResponseDto, lendingApplication, lenderAssociationDetailsRequest))
                             return true;
