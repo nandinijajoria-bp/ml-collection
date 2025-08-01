00fb1b688bbbdad0ac159391f27251f9878d5ba7
Vyom Singh <Vyom.Singh@VyomSingh.local>
2025-08-01 16:46:52 +0530

    updated

--- src/main/java/com/bharatpe/lending/controller/LoanDetailsController.java
+++ src/main/java/com/bharatpe/lending/controller/LoanDetailsController.java
@@ -288,7 +288,7 @@ public class LoanDetailsController {
 			@RequestAttribute(required = false) BasicDetailsDto merchant,
 			@RequestHeader(value = "token", required = false) String token,
 			@RequestParam(required = false) Long merchantId,
-			@RequestParam(required = false) String clientIdentifier
+			@RequestHeader(value = "clientIdentifier", required = false) String clientIdentifier
 	) {
 		if (Objects.nonNull(merchant)){
 			merchantId = merchant.getId();
