2787b11a9dca7def3366355f40b52cf3ed48df71
Vyom Singh <Vyom.Singh@VyomSingh.local>
2025-08-04 18:54:50 +0530

    updated

--- src/main/java/com/bharatpe/lending/util/LoanUtil.java
+++ src/main/java/com/bharatpe/lending/util/LoanUtil.java
@@ -3078,7 +3078,7 @@ public class LoanUtil {
 		eligibleLoan.setIrr(lendingApplicationServiceV2.getApr(eligibleLoan.getEdiCount(), Double.valueOf(eligibleLoan.getEdi()), loanAmount, eligibleLoan.getMerchantId(), null));
 		eligibleLoan.setApr(lendingApplicationServiceV2.getApr(eligibleLoan.getEdiCount(), Double.valueOf(eligibleLoan.getEdi()), loanAmount - processingFee.intValue(), eligibleLoan.getMerchantId(), null));
 		logger.info("eligibleLoan values -> {}, {}, {}, {}, {}, {}", eligibleLoan.getApr(), eligibleLoan.getIrr(), eligibleLoan.getProcessingFee(), eligibleLoan.getRateOfInterest(), eligibleLoan.getRepayment(), eligibleLoan.getEdi());
-		//eligibleLoanDao.save(eligibleLoan);
+		eligibleLoanDao.save(eligibleLoan);
 	}
 
 
