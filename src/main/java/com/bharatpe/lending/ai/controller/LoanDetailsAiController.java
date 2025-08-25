c1a22ea802106b2a3180d9f7481fbba41121c059
Himanshu kumar <himanshu.kumar@bharatpemoney.com>
2025-08-25 12:48:10 +0530

    updated loan-details api

--- src/main/java/com/bharatpe/lending/ai/controller/LoanDetailsAiController.java
+++ src/main/java/com/bharatpe/lending/ai/controller/LoanDetailsAiController.java
@@ -1,18 +1,36 @@
 package com.bharatpe.lending.ai.controller;
 
+import com.bharatpe.lending.ai.dto.LoanDetailResponse;
+import com.bharatpe.lending.ai.services.ILonaApplicationService;
+import com.bharatpe.lending.common.service.merchant.dto.BasicDetailsDto;
+import com.bharatpe.lending.loanV2.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("ai/loan-details")
 public class LoanDetailsAiController {
 
-    @GetMapping(value = "/latest",produces = "application/json")
-    public ResponseEntity<Object> getApplicationDetail(@RequestParam Long merchantId) {
+    private final ILonaApplicationService  lonaApplicationService;
 
-        return ResponseEntity.ok("Loan application details for merchantId: " + merchantId);
+    @GetMapping(value = "/latest",produces = "application/json")
+    public ResponseEntity<ApiResponse<LoanDetailResponse>> getApplicationDetail(
+            @RequestAttribute BasicDetailsDto merchant,
+            @RequestParam(required = false) Long merchantId) {
+        if(merchant!=null){
+            merchantId=merchant.getId();
+        }
+        log.info("Request received to get loan application details for merchantId: {}", merchantId);
+        LoanDetailResponse loanDetailResponse = lonaApplicationService.getLoanApplicationDetails(merchantId);
+        log.info("Fetched loan application details for merchantId: {}, details are: {}", merchantId, loanDetailResponse);
+        return ResponseEntity.ok(new ApiResponse<>(loanDetailResponse));
     }
 }
--- src/main/java/com/bharatpe/lending/ai/services/ILonaApplicationService.java
+++ src/main/java/com/bharatpe/lending/ai/services/ILonaApplicationService.java
@@ -1,10 +1,7 @@
 package com.bharatpe.lending.ai.services;
 
-import com.bharatpe.lending.ai.dto.LoanApplicationDetail;
 import com.bharatpe.lending.ai.dto.LoanDetailResponse;
 
-import java.util.List;
-
 public interface ILonaApplicationService {
-    public LoanDetailResponse getLoanApplicationDetails(Long merchantId);
+    LoanDetailResponse getLoanApplicationDetails(Long merchantId);
 }
