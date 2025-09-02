b2e42b1d840598ec1ac80c7530e0b56f511a97a6
kartikey <kartikey.parashar.exe@bharatpe.com>
2025-09-02 14:53:28 +0530

    add kfs details

---
+++ src/main/java/com/bharatpe/lending/loanV2/dto/KfsDetailsDto.java
@@ -0,0 +1,27 @@
+package com.bharatpe.lending.loanV2.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class KfsDetailsDto {
+
+    private String lenderCorporateName;
+    private String lenderBusinessAddress;
+    private  String lenderContactName;
+    private String lenderContactEmail;
+    private String lenderContactNumber;
+    private String colenderCorporateName;
+    private String colenderBusinessAddress;
+    private String lenderGrievanceTime;
+    private Integer coolingOffDays;
+    private Double processingFeePercentage;
+    private Double processingFeePercentageWithoutGst;
+    private Double processingFeeWithoutGst;
+    private Double annualTurnover;
+    private Double disbursalAmount;
+
+
+
+}
