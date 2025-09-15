e4c569a3b03c8da30de2d29da55e475f8f84cfb0
Vyom.Singh <vyom.singh@bharatpe.com>
2025-09-15 11:13:27 +0530

    fixed

--- src/main/java/com/bharatpe/lending/loanV3/utils/NbfcUtils.java
+++ src/main/java/com/bharatpe/lending/loanV3/utils/NbfcUtils.java
@@ -156,7 +156,7 @@ public class NbfcUtils {
         boolean isApplicableForAggregationFlow = !ObjectUtils.isEmpty(loanUtil.getLenderAggregationScreen(lendingApplication.getId()));
         if (enableLenderChange) {
             log.info("changing lender for the application {}", lendingApplication.getId());
-            if(!Arrays.asList(LendingViewStates.SHOP_DETAILS_PAGE.name(), LendingViewStates.SHOP_PICTURES_PAGE.name(), LendingViewStates.KYC_PAGE.name(), LendingViewStates.LENDER_EVALUATION_PAGE.name()).contains(lendingApplicationDetails.getApplicationViewState())
+            if(!Arrays.asList(LendingViewStates.LENDER_AGGREGATION.name(),LendingViewStates.SHOP_DETAILS_PAGE.name(), LendingViewStates.SHOP_PICTURES_PAGE.name(), LendingViewStates.KYC_PAGE.name(), LendingViewStates.LENDER_EVALUATION_PAGE.name()).contains(lendingApplicationDetails.getApplicationViewState())
                     || !ObjectUtils.isEmpty(lendingApplication.getAgreementAt())) {
                 log.info("skipping lender change and rejecting application as agreement already done / lendingViewState {} for application is not correct for applicationId {}", lendingApplicationDetails.getApplicationViewState(), lendingApplication.getId());
                 lendingApplication.setStatus("rejected");
