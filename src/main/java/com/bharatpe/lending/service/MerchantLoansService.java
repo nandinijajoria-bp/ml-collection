8026c6fa4c029b56fafa791193c21e4a07871319
RitikaNainwal-27 <ritika.nainwal@bharatpemoney.com>
2025-09-15 15:08:21 +0530

    feature-ML-2075 (#937)
    * topup changes
    
    * topup
    
    ---------
    
    Co-authored-by: Sanskarrawat <105841194+Sanskarrawat@users.noreply.github.com>
    Co-authored-by: anushank-varshney <165634134+anushank-varshney@users.noreply.github.com>

--- src/main/java/com/bharatpe/lending/service/MerchantLoansService.java
+++ src/main/java/com/bharatpe/lending/service/MerchantLoansService.java
@@ -770,7 +770,11 @@ public class MerchantLoansService {
         }
     }
 
-    private void setTopupDetailsV2(Long merchantId, @NotNull List<LoanEligibilityDTO> loans, TopupEligibilityResponseData responseDTO, LendingPaymentScheduleSlave lendingPaymentSchedule) throws Exception {
+    private void setTopupDetailsV2(Long merchantId, List<LoanEligibilityDTO> loans, TopupEligibilityResponseData responseDTO, LendingPaymentScheduleSlave lendingPaymentSchedule) throws Exception {
+        if (ObjectUtils.isEmpty(loans)) {
+            log.info("No loan eligibility data found for merchant {}", merchantId);
+            return;
+        }
         List<LoanEligibilityDTO> rejectedLoans = loans.stream()
                 .filter(dto -> BooleanUtils.isTrue(dto.getIsRejected()))
                 .collect(Collectors.toList());
