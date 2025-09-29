862e23f71b0c67b96929b7fdd29796dad2bc2856
Vyom.Singh <vyom.singh@bharatpe.com>
2025-09-29 13:38:05 +0530

    updated

--- src/main/java/com/bharatpe/lending/ai/services/AILedgerService.java
+++ src/main/java/com/bharatpe/lending/ai/services/AILedgerService.java
@@ -37,27 +37,27 @@ public class AILedgerService {
     public LedgerApiResponse fetchLedger(Long merchantId, String token) {
         String url = "http://localhost:9091/ai/collection/ledger?merchantId=" + merchantId;
 
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.set("token", token);
-//
-//        HttpEntity<String> entity = new HttpEntity(headers);
-//        try{
-//            log.info("Fetching ledger for merchantId: {}", merchantId);
-//
-//            ResponseEntity<LedgerApiResponse> responseEntity =
-//                    restTemplate.exchange(url, HttpMethod.GET, entity, LedgerApiResponse.class);
-//            log.info("Received response from ledger service for merchantId: {}, is {}", merchantId, responseEntity);
-//            LedgerApiResponse response = responseEntity.getBody();
-//            if(response!=null && response.getData() !=null){
-//                return null;
-//            }
-//        }
-//        catch (RestClientException e){
-//            log.error("Error while fetching ledger for merchantId: {}, error: {}", merchantId, e.getMessage());
-//        }catch (Exception e){
-//            log.error("Unknown error while fetching ledger for merchantId: {}, error: {}", merchantId, e.getMessage());
-//        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("token", token);
+
+        HttpEntity<String> entity = new HttpEntity(headers);
+        try{
+            log.info("Fetching ledger for merchantId: {}", merchantId);
+
+            ResponseEntity<LedgerApiResponse> responseEntity =
+                    restTemplate.exchange(url, HttpMethod.GET, entity, LedgerApiResponse.class);
+            log.info("Received response from ledger service for merchantId: {}, is {}", merchantId, responseEntity);
+            LedgerApiResponse response = responseEntity.getBody();
+            if(response!=null && response.getData() !=null){
+                return null;
+            }
+        }
+        catch (RestClientException e){
+            log.error("Error while fetching ledger for merchantId: {}, error: {}", merchantId, e.getMessage());
+        }catch (Exception e){
+            log.error("Unknown error while fetching ledger for merchantId: {}, error: {}", merchantId, e.getMessage());
+        }
 
         List<LendingPaymentSchedule> lendingPaymentScheduleList = lendingPaymentScheduleDao.findAllLendingPaymentScheduleByMerchantId(merchantId);
         if (lendingPaymentScheduleList.isEmpty()) {
